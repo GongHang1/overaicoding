anomalyco/opencode

feat: add subagent sidebar with hover navigation and project rename
- Rename project from opencode to overaicoding (package.json, bin, build)
- Add session tree utilities (session-tree.ts, child-session-picker.ts)
- Redesign header: compact single-row layout with subagent count and nav buttons
- Rewrite sidebar: root-session anchoring to prevent flicker during hover nav
- Add hover-to-navigate on subagent items and parent link in sidebar
- Add collapsible Subagents section with dropdown toggle
- Add child session list dialog and session_child_list keybind
- Enhance dialog-subagent with root/parent navigation options
- Keep sidebar visible when viewing subagent sessions

packages/opencode/src/config/config.ts
@@ -906,6 +906,7 @@ export namespace Config {
       session_child_cycle: z.string().optional().default("<leader>right").describe("Next child session"),
       session_child_cycle_reverse: z.string().optional().default("<leader>left").describe("Previous child session"),
       session_parent: z.string().optional().default("<leader>up").describe("Go to parent session"),
+      session_child_list: z.string().optional().default("<leader>down").describe("List child sessions"),
       terminal_suspend: z.string().optional().default("ctrl+z").describe("Suspend terminal"),
       terminal_title_toggle: z.string().optional().default("none").describe("Toggle terminal title"),
       tips_toggle: z.string().optional().default("<leader>h").describe("Toggle tips on home screen"),

GongHang1/overaicoding

packages/opencode/src/config/config.ts
@@ -808,6 +808,7 @@ export namespace Config {
       session_child_cycle: z.string().optional().default("right").describe("Go to next child session"),
       session_child_cycle_reverse: z.string().optional().default("left").describe("Go to previous child session"),
       session_parent: z.string().optional().default("up").describe("Go to parent session"),
+      session_child_list: z.string().optional().default("<leader>l").describe("List child sessions"),
       terminal_suspend: z.string().optional().default("ctrl+z").describe("Suspend terminal"),
       terminal_title_toggle: z.string().optional().default("none").describe("Toggle terminal title"),
       tips_toggle: z.string().optional().default("<leader>h").describe("Toggle tips on home screen"),